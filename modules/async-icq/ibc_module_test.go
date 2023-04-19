a4114621af1d76d08c95615273f6eb2abe8be895
jtieri <justin@thetieris.com>
2023-04-19 12:49:10 -0500

    chore: address linter errors

--- modules/async-icq/ibc_module_test.go
+++ modules/async-icq/ibc_module_test.go
@@ -25,6 +25,8 @@ var (
 
 	TestQueryPath = "/store/params/key"
 	TestQueryData = "icqhost/HostEnabled"
+
+	version = "version"
 )
 
 type InterchainQueriesTestSuite struct {
@@ -111,7 +113,7 @@ func (suite *InterchainQueriesTestSuite) TestOnChanOpenInit() {
 		},
 		{
 			"invalid version", func() {
-				channel.Version = "version"
+				channel.Version = version
 			}, false,
 		},
 		{
@@ -191,7 +193,7 @@ func (suite *InterchainQueriesTestSuite) TestOnChanOpenTry() {
 		},
 		{
 			"invalid counterparty version", func() {
-				counterpartyVersion = "version"
+				counterpartyVersion = version
 			}, false,
 		},
 	}
@@ -255,7 +257,7 @@ func (suite *InterchainQueriesTestSuite) TestOnChanOpenAck() {
 		},
 		{
 			"invalid counterparty version", func() {
-				counterpartyVersion = "version"
+				counterpartyVersion = version
 			}, false,
 		},
 	}
