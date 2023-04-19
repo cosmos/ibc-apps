1d52398d7a916bc7b30d8b78693502f154de1842
jtieri <justin@thetieris.com>
2023-04-19 12:38:22 -0500

    feat: migrate async-icq v4 to repo

---
+++ modules/async-icq/keeper/events.go
@@ -0,0 +1,20 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	icqtypes "github.com/cosmos/ibc-apps/modules/async-icq/v4/types"
+	"github.com/cosmos/ibc-go/v4/modules/core/exported"
+)
+
+// EmitWriteErrorAcknowledgementEvent emits an event signalling an error acknowledgement and including the error details
+func EmitWriteErrorAcknowledgementEvent(ctx sdk.Context, packet exported.PacketI, err error) {
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			icqtypes.EventTypePacketError,
+			sdk.NewAttribute(sdk.AttributeKeyModule, icqtypes.ModuleName),
+			sdk.NewAttribute(icqtypes.AttributeKeyAckError, err.Error()),
+			sdk.NewAttribute(icqtypes.AttributeKeyHostChannelID, packet.GetDestChannel()),
+		),
+	)
+}
