75b861652125474c1df72c4bbe76f4bd7a3dbf2c
Justin Tieri <37750742+jtieri@users.noreply.github.com>
2023-06-28 15:07:30 -0500

    feat: bring in v4 of the pfm (#39)
    * feat: bring in v4 of the pfm & fix linter issues
    
    * docs: update LICENSE

---
+++ middleware/packet-forward-middleware/router/keeper/grpc_query.go
@@ -0,0 +1,20 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v4/router/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+var _ types.QueryServer = Keeper{}
+
+func (k Keeper) Params(c context.Context, _ *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
+	ctx := sdk.UnwrapSDKContext(c)
+	params := k.GetParams(ctx)
+
+	return &types.QueryParamsResponse{
+		Params: &params,
+	}, nil
+}
---
+++ middleware/packet-forward-middleware/router/types/expected_keepers.go
@@ -0,0 +1,37 @@
+package types
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+
+	"github.com/cosmos/ibc-go/v4/modules/apps/transfer/types"
+	channeltypes "github.com/cosmos/ibc-go/v4/modules/core/04-channel/types"
+)
+
+// TransferKeeper defines the expected transfer keeper
+type TransferKeeper interface {
+	Transfer(ctx context.Context, msg *types.MsgTransfer) (*types.MsgTransferResponse, error)
+	DenomPathFromHash(ctx sdk.Context, denom string) (string, error)
+}
+
+// ChannelKeeper defines the expected IBC channel keeper
+type ChannelKeeper interface {
+	GetChannel(ctx sdk.Context, srcPort, srcChan string) (channel channeltypes.Channel, found bool)
+	GetPacketCommitment(ctx sdk.Context, portID, channelID string, sequence uint64) []byte
+	GetNextSequenceSend(ctx sdk.Context, portID, channelID string) (uint64, bool)
+	LookupModuleByChannel(ctx sdk.Context, portID, channelID string) (string, *capabilitytypes.Capability, error)
+}
+
+// DistributionKeeper defines the expected distribution keeper
+type DistributionKeeper interface {
+	FundCommunityPool(ctx sdk.Context, amount sdk.Coins, sender sdk.AccAddress) error
+}
+
+// BankKeeper defines the expected bank keeper
+type BankKeeper interface {
+	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	BurnCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+}
