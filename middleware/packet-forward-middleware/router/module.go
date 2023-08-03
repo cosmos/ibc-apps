21ffbf4bd5a7a668c564bff5129d58ebbcc8ada0
jtieri <justin@thetieris.com>
2023-08-03 13:41:22 -0500

    fix: revert changes accidentally introduced in 2e491b90e22e53870c4b6a3e2fa2977571bcd56d

--- middleware/packet-forward-middleware/router/module.go
+++ middleware/packet-forward-middleware/router/module.go
@@ -22,9 +22,6 @@ import (
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 )
 
-// ConsensusVersion defines the current packetforwardmiddleware module consensus version.
-const ConsensusVersion = 2
-
 var (
 	_ module.AppModule           = AppModule{}
 	_ module.AppModuleBasic      = AppModuleBasic{}
@@ -132,7 +129,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements AppModule/ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return ConsensusVersion }
+func (AppModule) ConsensusVersion() uint64 { return 1 }
 
 // BeginBlock implements the AppModule interface
 func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
