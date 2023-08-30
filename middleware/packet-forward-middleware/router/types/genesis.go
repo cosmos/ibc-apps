6009b8794852123107c4cf699b64ed119c2691d4
jtieri <justin@thetieris.com>
2023-08-30 15:54:41 -0500

    fix: validate proposed genesis state in InitGenesis

--- middleware/packet-forward-middleware/router/types/genesis.go
+++ middleware/packet-forward-middleware/router/types/genesis.go
@@ -1,6 +1,6 @@
 package types
 
-// NewGenesisState creates a 29-fee GenesisState instance.
+// NewGenesisState creates a pfm GenesisState instance.
 func NewGenesisState(params Params, inFlightPackets map[string]InFlightPacket) *GenesisState {
 	return &GenesisState{
 		Params:          params,
@@ -8,7 +8,7 @@ func NewGenesisState(params Params, inFlightPackets map[string]InFlightPacket) *
 	}
 }
 
-// DefaultGenesisState returns a GenesisState with "transfer" as the default PortID.
+// DefaultGenesisState returns a GenesisState with a default fee percentage of 0.
 func DefaultGenesisState() *GenesisState {
 	return &GenesisState{
 		Params:          DefaultParams(),
@@ -16,8 +16,7 @@ func DefaultGenesisState() *GenesisState {
 	}
 }
 
-// Validate performs basic genesis state validation returning an error upon any
-// failure.
+// Validate performs basic genesis state validation returning an error upon any failure.
 func (gs GenesisState) Validate() error {
 	return gs.Params.Validate()
 }
