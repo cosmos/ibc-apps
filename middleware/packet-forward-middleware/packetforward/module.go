9b5795d160888c658a2a6bb0d300dbffaf8678dc
swelf <golishkin@gmail.com>
2023-11-15 16:01:51 +0300

    fixed migration from version

--- middleware/packet-forward-middleware/packetforward/module.go
+++ middleware/packet-forward-middleware/packetforward/module.go
@@ -109,7 +109,7 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
 
 	m := keeper.NewMigrator(am.keeper, am.legacySubspace)
-	if err := cfg.RegisterMigration(types.ModuleName, 2, m.Migrate1to2); err != nil {
+	if err := cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1to2); err != nil {
 		panic(fmt.Sprintf("failed to migrate x/%s from version 1 to 2: %v", types.ModuleName, err))
 	}
 }
