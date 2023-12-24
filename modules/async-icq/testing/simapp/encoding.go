49adb41bb7768a2797a1b26561a8a4d3e16744ef
Jacob Gadikian <jacobgadikian@gmail.com>
2023-12-25 02:14:15 +0700

    add encoding.go

---
+++ modules/async-icq/testing/simapp/encoding.go
@@ -0,0 +1,37 @@
+//go:build !test_amino
+// +build !test_amino
+
+package simapp
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/tx"
+)
+
+// MakeTestEncodingConfig creates an EncodingConfig for a non-amino based test configuration.
+// This function should be used only internally (in the SDK).
+// App user should'nt create new codecs - use the app.AppCodec instead.
+// [DEPRECATED]
+func MakeTestEncodingConfig() EncodingConfig {
+	cdc := codec.NewLegacyAmino()
+	interfaceRegistry := types.NewInterfaceRegistry()
+	protoCdc := codec.NewProtoCodec(interfaceRegistry)
+
+	return EncodingConfig{
+		InterfaceRegistry: interfaceRegistry,
+		Codec:             protoCdc,
+		TxConfig:          tx.NewTxConfig(protoCdc, tx.DefaultSignModes),
+		Amino:             cdc,
+	}
+}
+
+// EncodingConfig specifies the concrete encoding types to use for a given app.
+// This is provided for compatibility between protobuf and amino implementations.
+type EncodingConfig struct {
+	InterfaceRegistry types.InterfaceRegistry
+	Codec             codec.Codec
+	TxConfig          client.TxConfig
+	Amino             *codec.LegacyAmino
+}
