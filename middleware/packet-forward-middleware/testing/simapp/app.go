fc818a2b9cd16f05502626ce5f0f9376b37a5ef5
Michael FIG <mfig@agoric.com>
2025-04-15 21:07:54 -0600

    test(simapp): pass `StakingKeeper` to `vesting`

--- middleware/packet-forward-middleware/testing/simapp/app.go
+++ middleware/packet-forward-middleware/testing/simapp/app.go
@@ -507,7 +507,7 @@ func NewSimApp(
 			encodingConfig.TxConfig,
 		),
 		auth.NewAppModule(appCodec, app.AccountKeeper, authsims.RandomGenesisAccounts, app.GetSubspace(authtypes.ModuleName)),
-		vesting.NewAppModule(app.AccountKeeper, app.BankKeeper),
+		vesting.NewAppModule(app.AccountKeeper, app.BankKeeper, app.StakingKeeper),
 		bank.NewAppModule(appCodec, app.BankKeeper, app.AccountKeeper, app.GetSubspace(banktypes.ModuleName)),
 		capability.NewAppModule(appCodec, *app.CapabilityKeeper, false),
 		crisis.NewAppModule(app.CrisisKeeper, skipGenesisInvariants, app.GetSubspace(crisistypes.ModuleName)),
